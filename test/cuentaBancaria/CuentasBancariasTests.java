a82ec286ed7fe9ad74e5de7f263a44aa1a3ffccb
Patricia <patriciasandagorda@gmail.com>
2017-09-07 17:07:22 -0300

    Rojo: Primera Prueba, Se debe generar clase

---
+++ test/cuentaBancaria/CuentasBancariasTests.java
@@ -0,0 +1,12 @@
+package cuentaBancaria;
+
+import org.junit.Test;
+
+public class CuentasBancariasTests {
+	
+	@Test
+	public void queInicializa(){
+		CuentaBancaria cuenta1=new CuentaBancaria();
+	}
+
+}
