d14382e00dba9e2fbdca31bcc1e145e6a65ce896
Patricia <patriciasandagorda@gmail.com>
2017-09-07 17:29:30 -0300

    Rojo: se transfieren 100 de cuenta origen a cuenta destino

--- test/cuentaBancaria/CuentasBancariasTests.java
+++ test/cuentaBancaria/CuentasBancariasTests.java
@@ -6,18 +6,26 @@ import org.junit.Test;
 import cuentasBancarias.CuentaBancaria;
 
 public class CuentasBancariasTests {
-	
+
 	@Test
-	public void queInicializa(){
-		CuentaBancaria cuenta1=new CuentaBancaria();
-		Assert.assertEquals(0, cuenta1.mostrar(),0);
+	public void queInicializa() {
+		CuentaBancaria cuenta1 = new CuentaBancaria();
+		Assert.assertEquals(0, cuenta1.mostrar(), 0);
+	}
+
+	@Test
+	public void queSeLePuedeAgregarDinero() {
+		CuentaBancaria cuenta1 = new CuentaBancaria();
+		cuenta1.depositar(1000);
+		Assert.assertEquals(1000, cuenta1.mostrar(), 0);
 	}
 	
 	@Test
-	public void queSeLePuedeAgregarDinero(){
-	CuentaBancaria cuenta1=new CuentaBancaria();
-	cuenta1.depositar(1000);
-	Assert.assertEquals(1000, cuenta1.mostrar(),0);
-
+	public void queSePuedeTransferirAOtraCuenta() {
+		CuentaBancaria cuenta1 = new CuentaBancaria();
+		CuentaBancaria dest = new CuentaBancaria();
+		cuenta1.depositar(1000);
+		cuenta1.transferirMontohacia(100, dest);
+		Assert.assertEquals(100, dest.mostrar(), 0);
 	}
 }
